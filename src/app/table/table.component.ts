7332e6a128c246271cea1faa1ea4a5b5dd27ddd4
Igor Eskin <eskinhd@gmail.com>
2019-02-20 15:06:31 -0500

    Local storage seems to be working

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,23 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
+
 export class TableComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpService: HttpClient) { }
+  quoteinfo: string[];
+
 
   ngOnInit() {
     // localStorage.clear();
 
     if(localStorage.length == 0){
       alert("Please refresh the page to load the data!")
-      $.getJSON("../../seed-data.json", function(json) {
-          localStorage.setItem('quoteinfo', JSON.stringify(json))
-      });
-  }
+
+      this.httpService.get("../assets/seed-data.json").subscribe(
+        data => {
+          this.quoteinfo = data as string[];
+          console.log(this.quoteinfo[0]);
+          console.log(JSON.stringify(this.quoteinfo))
+          localStorage.setItem('quotes', JSON.stringify(this.quoteinfo))
+        },
+        (err: HttpErrorResponse) => {
+          console.log(err.message);
+        }
+      )
+      // localStorage.setItem('quotes', JSON.stringify(this.quoteinfo))
+
+      // $.getJSON("../../seed-data.json", function(json) {
+      //     localStorage.setItem('quoteinfo', JSON.stringify(json))
+      // });
+    } else {
+      console.log(localStorage.length)
+    }
   }
 
 }
