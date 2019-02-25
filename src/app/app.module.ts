a4e39e969d856cba2a528c310b67bf21a6b209a1
Igor Eskin <eskinhd@gmail.com>
2019-02-25 08:00:48 -0500

    Some corrections in the entry form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
 // import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -26,6 +27,7 @@ import { EntryformComponent } from './entryform/entryform.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     // RouterModule,
     AppRoutingModule,
     BsDatepickerModule.forRoot(),
--- src/app/entryform/entryform.component.ts
+++ src/app/entryform/entryform.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-entryform',
@@ -6,6 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entryform.component.css']
 })
 export class EntryformComponent implements OnInit {
+  @ViewChild('f') entryForm: NgForm;
+
+  quotetype = '';
+  quotenumber = '';
+  tasktype = '';
+  contactname = '';
+  quote = '';
+  duedate = '';
+  taskdescription = '';
+  submitted = false;
+
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form)
+  // }
+
+  onSubmit() {
+    this.submitted = true;
+    
+    this.quotetype = this.entryForm.value.quotetype;
+    this.quotenumber = this.entryForm.value.quotenumber;
+    this.tasktype = this.entryForm.value.tasktype;
+    this.contactname = this.entryForm.value.contactname;
+    this.quote = this.entryForm.value.quote;
+    this.duedate = this.entryForm.value.duedate;
+    this.taskdescription = this.entryForm.value.taskdescription;
+
+    this.entryForm.reset();
+  }
 
   constructor() { }
 
